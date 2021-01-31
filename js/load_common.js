f7b08aa719e953199e227b400abca0489b72872e
Finlay <fintcm@gmail.com>
2021-01-31 12:01:45 -0700

    remove .html from links

--- js/load_common.js
+++ js/load_common.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#nav_container').load('common/nav.html');
-    $('#footer_container').load('common/footer.html');
+    $('#nav_container').load('./common/nav.html');
+    $('#footer_container').load('./common/footer.html');
 });
 
